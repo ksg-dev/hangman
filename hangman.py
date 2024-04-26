422923e63607e5e203978f5ff9f49f0928665098
ksg-dev <ksg.dev.data@gmail.com>
2024-04-26 16:25:05 -0500

    hangman part two logic

--- hangman.py
+++ hangman.py
@@ -1,7 +1,5 @@
 import random
 
-#Step 1 
-
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
@@ -14,8 +12,36 @@ guess = input("Guess a letter: ").lower().strip()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
+
+
+#Step 2
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+
+display = []
+for i in range(len(chosen_word)):
+    display.append("_")
+
+
+#TODO-2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+slot = 0
 for i in chosen_word:
     if guess == i:
         print("Right")
+        display[slot] = i
+        slot += 1
     else:
         print("Wrong")
+        slot += 1
+
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
+print(display)
